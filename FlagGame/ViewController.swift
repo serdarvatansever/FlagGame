f81920fb06e8777fc0b074e351a42a52269fae31
Serdar Vatansever <serdarvatansever@serdar-macbook-pro.local>
2018-10-08 14:13:55 +0300

    Initial Commit

---
+++ FlagGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagGame
+//
+//  Created by Serdar Vatansever on 8.10.2018.
+//  Copyright © 2018 Serdar Vatansever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
